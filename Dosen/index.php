23e12de416b876d0b8314f6e272f8dc47e43fd5e
Kazutodp <larbiansyahd@gmail.com>
2026-05-18 00:09:12 +0800

    pembaruan

--- Dosen/index.php
+++ Dosen/index.php
@@ -16,6 +16,7 @@ $query = mysqli_query($koneksi, "SELECT * FROM dosen");
         </style>
     </head>
     <body>
+        <a href="../dashboard.php" style="text-decoration: none; color: #555; font-weight: bold;">⬅ Kembali ke Dashboard</a>
         <h2>Daftar Dosen Pengampu</h2>
         <a href="tambah.php" style="text-decoration: none; background: #2196F3; color: white; padding: 8px 15px; border-radius: 5px;">[+] Tambah Dosen</a>
 
